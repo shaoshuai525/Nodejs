320516d4b75644b04b5fe4ec0d5babbd1b2078b7
shaoshuai525 <244611758@qq.com>
2021-11-03 22:35:03 +0800

    1-3.Node的模块化细节

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-// console.log(__dirname);
+// require("C:\\Users\\ALW-15\\Desktop\\我的\\NodeJs\\1.Node核心\\1-3.Node的模块化细节\\a.js")
 
-// console.log("当前命令行：", process.cwd());
+// const result = require("./b.json");
+// console.log(result);
 
-// setTimeout(() => {
-//   console.log("abc");
-// }, 1000);
+// console.log(require);
+// 强制转换成绝对路径
+// console.log("绝对路径为：",require.resolve("./src"));
 
-// process.exit();
 
-// 获取命令中的所有参数
-// console.log(process.argv);
+// 以下代码暂时不完全，需要日后再次看看，看明白后再补全
+function require(modulePath) {
+  // 1. 将modulePath转换为绝对路径
+  // 2. 判断是否该模块已有缓存
+  if (require.cache["绝对路径"]) {
+    return "模块的运行结果";
+  }
+  // 3. 读取文件内容
+  // 4. 包裹到一个函数中
+}
 
-// 获取当前操作系统
-// console.log(process.platform);
-
-// 根据进程id杀死进程
-// process.kill(12148);
-
-// 获取环境变量
-console.log(process.env);
+const result = require("./myModule");
+console.log(result);
